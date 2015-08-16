29ed0617b8157a37a3c1c3be85ba3765b595686c
Michael Koppen <michael.koppen@googlemail.com>
2015-08-16 14:27:14 +0200

    openshift travis connection

--- build_openshift_binary.sh
+++ build_openshift_binary.sh
@@ -2,7 +2,7 @@
 
 ls -la
 
-/usr/local/bin/mvn clean install -DskipTests=true -Dmaven.javadoc.skip=true -Dhome.database.driver.classname=driver -Dhome.database.url=adress -Dhome.database.username=username -Dhome.database.password=password -Popenshift -B -V
+/usr/local/bin/mvn clean install -DskipTests=true -Dmaven.javadoc.skip=true -Dbase.database.driver.classname=driver -Dbase.database.url=adress -Dbase.database.username=username -Dbase.database.password=password -Popenshift -B -V
 
 # Use war name 'ROOT.war' if you would like to deploy to the root path of your server
 cp webapps/ROOT.war binary/dependencies/jbossews/webapps/ROOT.war
